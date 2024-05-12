3c5cb93a6c162416f0eb8e5dd634b771f74eae2b
Konnor Rogers <konnor5456@gmail.com>
2024-05-12 00:40:17 -0400

    comment out tests for tooltip

--- tests/tooltip.test.js
+++ tests/tooltip.test.js
@@ -1,47 +1,47 @@
-import "../exports/tooltip/tooltip-register.js"
-import { fixture } from "@open-wc/testing"
+// import "../exports/tooltip/tooltip-register.js"
+// import { fixture } from "@open-wc/testing"
 
-test("Should show on hover and hide on mouseout", async () => {
-  const container = await fixture(html`
-    <div>
-      <button data-role-tooltip="tooltip">I'm a button.</button>
-      <role-tooltip id="tooltip">My tooltip</role-tooltip>
-    </div>
-  `)
+// test("Should show on hover and hide on mouseout", async () => {
+//   const container = await fixture(html`
+//     <div>
+//       <button data-role-tooltip="tooltip">I'm a button.</button>
+//       <role-tooltip id="tooltip">My tooltip</role-tooltip>
+//     </div>
+//   `)
 
-  const button = container.querySelector("button")
-  const tooltip = container.querySelector("role-tooltip")
-})
+//   const button = container.querySelector("button")
+//   const tooltip = container.querySelector("role-tooltip")
+// })
 
-test("Should show on focus and hide on blur", async () => {
-  const container = await fixture(html`
-    <div>
-      <button data-role-tooltip="tooltip">I'm a button.</button>
-      <role-tooltip id="tooltip">My tooltip</role-tooltip>
-    </div>
-  `)
-  const button = container.querySelector("button")
-  const tooltip = container.querySelector("role-tooltip")
-})
+// test("Should show on focus and hide on blur", async () => {
+//   const container = await fixture(html`
+//     <div>
+//       <button data-role-tooltip="tooltip">I'm a button.</button>
+//       <role-tooltip id="tooltip">My tooltip</role-tooltip>
+//     </div>
+//   `)
+//   const button = container.querySelector("button")
+//   const tooltip = container.querySelector("role-tooltip")
+// })
 
-test("Should show on focus and hide click", async () => {
-  const container = await fixture(html`
-    <div>
-      <button data-role-tooltip="tooltip">I'm a button.</button>
-      <role-tooltip id="tooltip">My tooltip</role-tooltip>
-    </div>
-  `)
-  const button = container.querySelector("button")
-  const tooltip = container.querySelector("role-tooltip")
-})
+// test("Should show on focus and hide click", async () => {
+//   const container = await fixture(html`
+//     <div>
+//       <button data-role-tooltip="tooltip">I'm a button.</button>
+//       <role-tooltip id="tooltip">My tooltip</role-tooltip>
+//     </div>
+//   `)
+//   const button = container.querySelector("button")
+//   const tooltip = container.querySelector("role-tooltip")
+// })
 
-test("Should show on hover and hide click", async () => {
-  const container = await fixture(html`
-    <div>
-      <button data-role-tooltip="tooltip">I'm a button.</button>
-      <role-tooltip id="tooltip">My tooltip</role-tooltip>
-    </div>
-  `)
-  const button = container.querySelector("button")
-  const tooltip = container.querySelector("role-tooltip")
-})
+// test("Should show on hover and hide click", async () => {
+//   const container = await fixture(html`
+//     <div>
+//       <button data-role-tooltip="tooltip">I'm a button.</button>
+//       <role-tooltip id="tooltip">My tooltip</role-tooltip>
+//     </div>
+//   `)
+//   const button = container.querySelector("button")
+//   const tooltip = container.querySelector("role-tooltip")
+// })
